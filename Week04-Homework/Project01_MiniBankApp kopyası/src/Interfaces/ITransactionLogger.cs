5e9cb34a874814092f82e7015e001764b5808ed1
Esma <dnresmaa@gmail.com>
2026-08-09 17:06:53 +0300

    Gereksiz kopya klasör silindi

--- Week04-Homework/Project01_MiniBankApp kopyası/src/Interfaces/ITransactionLogger.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Project01_MiniBankApp.src.Interfaces;
-
-public interface ITransactionLogger
-{
-    void Log(Guid accountId, string operation, decimal amount, decimal balanceAfter); //Yeni bir işlem gerçekleştiğinde işlemin her detayını kaydedecek
-    List<string> GetHistory(Guid accountId); //Hesabın işlem geçmişini görüntüleyecek
-}
